81d9f1f3a93e29c6640b28895d2d3a73d13af226
ThakurVibha <vibhathakur39@gmail.com>
2021-12-08 17:21:05 -0800

    learn scaffold and hot reload

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+        body: Image(image: AssetImage('images/hourglass.png')),
+    backgroundColor: Colors.white,
+    appBar: AppBar(
+    title: Text("First Flutter app"),
+    backgroundColor: Colors.blue,
+    ),
+    floatingActionButton: FloatingActionButton(
+    child: Text('V'),
+    onPressed: () {},
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+    items: [
+    BottomNavigationBarItem(
+    label: 'Home',
+    icon:Icon(Icons.ten_k_rounded)
+
+    ),
+    BottomNavigationBarItem(
+    label: "Settings",
+    icon: Icon(Icons.ten_mp_outlined)
+    ),
+
+
+    ],
+    ),
+    drawer: Drawer(
+    child: Image(
+    image: AssetImage('images/delievery.png'),
+    ),
+    ),
+    ),
+    );
+  }
+}
+
+
+
+
